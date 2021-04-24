9ad6dbf913204854b22b9d4b86e3f29599e0be55
Max16 <max16@naver.com>
2021-04-24 21:05:29 +0900

    #5.2 Chat part One

--- assets/js/notification.js
+++ assets/js/notification.js
@@ -1,16 +1,15 @@
-const notifications = document.getElementById("jsNotifications");
+const body = document.querySelector("body");
 
 const fireNotification = (text, color) => {
   const notification = document.createElement("div");
   notification.innerText = text;
   notification.style.backgroundColor = color;
-  notifications.appendChild(notification);
+  notification.className = "notification";
+  body.appendChild(notification);
 };
 
-export const handleNewUser = ({ nickname }) => {
+export const handleNewUser = ({ nickname }) =>
   fireNotification(`${nickname} just joined!`, "rgb(0, 122, 255)");
-};
 
-export const handleDisconnected = ({ nickname }) => {
+export const handleDisconnected = ({ nickname }) =>
   fireNotification(`${nickname} just left!`, "rgb(255, 149, 0)");
-};
