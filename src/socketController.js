45f1f72871589c9445718fb7a559c5c100ed2bfa
Max16 <max16@naver.com>
2021-04-25 20:59:19 +0900

    #6.3 Game Programming part One

--- src/socketController.js
+++ src/socketController.js
@@ -1,18 +1,31 @@
 import events from "./events";
+import { chooseWords } from "./word";
 
 let sockets = [];
+let inProgress = false;
+let word = null;
+
+const chooseLeader = () => sockets[Math.floor(Math.random * sockets.length)];
 
 const socketController = (socket, io) => {
   const broadcast = (event, data) => socket.broadcast.emit(event, data);
   const superBroadcast = (event, data) => io.emit(event, data); // io 를 사용하여 자기 socket 을 포함한 모든 socket 에게 알림(공지사항)
   const sendPlayerUpdate = () =>
     superBroadcast(events.playerUpdate, { sockets });
+  const startGame = () => {
+    if (inProgress === false) {
+      inProgress = true;
+      const leader = chooseLeader();
+      word = chooseWords();
+    }
+  };
 
   socket.on(events.setNickname, ({ nickname }) => {
     socket.nickname = nickname;
     sockets.push({ id: socket.id, points: 0, nickname }); // 필요시 DB에 저장
     broadcast(events.newUser, { nickname });
     sendPlayerUpdate();
+    startGame();
   });
   socket.on(events.disconnect, () => {
     sockets = sockets.filter((aSocket) => aSocket.id !== socket.id);
